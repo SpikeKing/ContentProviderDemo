1dda48c16b5e5a7436e11882c0274a84f37b54b8
spike <morndragon@126.com>
2016-06-15 14:24:30 +0800

    修改显示

--- app/src/main/java/org/wangchenlong/contentproviderdemo/MainActivity.java
+++ app/src/main/java/org/wangchenlong/contentproviderdemo/MainActivity.java
@@ -6,10 +6,15 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "DEBUG-WCL: " + MainActivity.class.getSimpleName();
+
+    private TextView mTvShowBooks; // 显示书籍
+    private TextView mTvShowUsers; // 显示用户
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +22,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Uri uri = Uri.parse("content://org.wangchenlong.bookprovider");
-//        ContentResolver resolver = getContentResolver();
-//        resolver.query(uri, null, null, null, null);
-//        resolver.query(uri, null, null, null, null);
-//        resolver.query(uri, null, null, null, null);
+        mTvShowBooks = (TextView) findViewById(R.id.main_tv_show_books);
+        mTvShowUsers = (TextView) findViewById(R.id.main_tv_show_users);
 
+//        Uri bookUri = BookProvider.BOOK_CONTENT_URI;
+//        Cursor bookCursor = getContentResolver().query(bookUri, new String[]{"_id", "name"}, null, null, null);
+//        if (bookCursor != null) {
+//            while (bookCursor.moveToNext()) {
+//                Book book = new Book();
+//                book.bookId = bookCursor.getInt(0);
+//                book.bookName = bookCursor.getString(1);
+//                Log.e(TAG, "query book: " + book.toString());
+//            }
+//            bookCursor.close();
+//        }
+
+//        Uri userUri = BookProvider.USER_CONTENT_URI;
+//        Cursor userCursor = getContentResolver().query(userUri, new String[]{"_id", "name", "sex"}, null, null, null);
+//        if (userCursor != null) {
+//            while (userCursor.moveToNext()) {
+//                User user = new User();
+//                user.userId = userCursor.getInt(0);
+//                user.userName = userCursor.getString(1);
+//                user.isMale = userCursor.getInt(2) == 1;
+//                Log.e(TAG, "query user:" + user.toString());
+//            }
+//            userCursor.close();
+//        }
+    }
+
+    /**
+     * 添加书籍的事件监听
+     *
+     * @param view 视图
+     */
+    public void addBooks(View view) {
         Uri bookUri = BookProvider.BOOK_CONTENT_URI;
         ContentValues values = new ContentValues();
         values.put("_id", 6);
         values.put("name", "信仰上帝");
         getContentResolver().insert(bookUri, values);
+    }
+
+    /**
+     * 显示书籍
+     *
+     * @param view 视图
+     */
+    public void showBooks(View view) {
+        String content = "";
+        Uri bookUri = BookProvider.BOOK_CONTENT_URI;
         Cursor bookCursor = getContentResolver().query(bookUri, new String[]{"_id", "name"}, null, null, null);
         if (bookCursor != null) {
             while (bookCursor.moveToNext()) {
                 Book book = new Book();
                 book.bookId = bookCursor.getInt(0);
                 book.bookName = bookCursor.getString(1);
+                content += book.toString() + "\n";
                 Log.e(TAG, "query book: " + book.toString());
+                mTvShowBooks.setText(content);
             }
             bookCursor.close();
         }
+    }
 
+    /**
+     * 显示用户
+     *
+     * @param view 视图
+     */
+    public void showUsers(View view) {
+        String content = "";
         Uri userUri = BookProvider.USER_CONTENT_URI;
         Cursor userCursor = getContentResolver().query(userUri, new String[]{"_id", "name", "sex"}, null, null, null);
         if (userCursor != null) {
@@ -47,7 +101,9 @@ public class MainActivity extends AppCompatActivity {
                 user.userId = userCursor.getInt(0);
                 user.userName = userCursor.getString(1);
                 user.isMale = userCursor.getInt(2) == 1;
+                content += user.toString() + "\n";
                 Log.e(TAG, "query user:" + user.toString());
+                mTvShowUsers.setText(content);
             }
             userCursor.close();
         }
